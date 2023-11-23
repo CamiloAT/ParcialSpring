50cbb6d0681e12c0e3733a3c6631852595fd9fe2
KarenSuarez4 <karen.pena04@uptc.edu.co>
2023-11-23 16:48:50 -0500

    Se añade appointment

---
+++ src/main/java/co/edu/uptc/PetShop/model/Appointment.java
@@ -0,0 +1,41 @@
+package co.edu.uptc.PetShop.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "appointment")
+public class Appointment {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column
+    private String nameVet;
+
+    @Column
+    private String namePet;
+
+    @Column
+    private String date;
+
+    @Column
+    private String diagnosis;
+
+    @Column
+    private String treatmenti;
+
+    @OneToOne(mappedBy = "apointment", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Invoice invoice;
+
+
+
+}

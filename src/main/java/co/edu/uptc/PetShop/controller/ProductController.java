b3a92df7401fbd47609c70e1e9d13e8020752815
Pedro Cruz <pedro.cruz.lopez001@gmail.com>
2023-11-23 17:28:05 -0500

    .

---
+++ src/main/java/co/edu/uptc/PetShop/controller/ProductController.java
@@ -0,0 +1,24 @@
+package co.edu.uptc.PetShop.controller;
+
+import co.edu.uptc.PetShop.model.Product;
+import co.edu.uptc.PetShop.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/appointment")
+@RestController
+@Validated
+public class ProductController {
+        @Autowired
+        private ProductService productService;
+        @PostMapping("save")
+        public ResponseEntity<Product> saveProduct(@RequestBody Product product) {
+            Product newProduct = this.productService.save(product);
+            return ResponseEntity.ok(newProduct);
+        }
+}
